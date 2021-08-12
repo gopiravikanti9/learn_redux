ba7611d3d2130a94c679cf490688b7470786b69f
GOPICHAND Ravikanti <ravikanti.gopichand@soprasteria.com>
2021-08-12 19:35:35 +0530

    combineReducers in Redux

--- src/index.js
+++ src/index.js
@@ -2,48 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';//combine reducers in order to add multiple reducers in a single store
+import allReducers from "./Reducers/index"
+import { Provider } from "react-redux";
+
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
-//Store well the store is a place to store data as
-//action
-
-const increment = () => {
-  return {
-    type: "INCREMENT"
-  }
-}
-const decrement = () => {
-  return {
-    type: "DECREMENT"
-
-  }
-}
-//reducer will tell us what is supposed when we perform an action
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1
-    case "DECREMENT":
-      return state - 1
-
-  }
-}//store
-let store = createStore(counter)
-//to display our counter in the display window
-store.subscribe(() => console.log(store.getState()));
-// dispatcher
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
